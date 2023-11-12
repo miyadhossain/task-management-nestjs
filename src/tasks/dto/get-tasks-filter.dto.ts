83dfbaf0b98302126a6ec59377453307f8924a72
miyadhossain <miyad.mh@gmail.com>
2023-11-12 15:02:06 +0600

    class validation and error handling added

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,11 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsString()
   search?: string;
 }
